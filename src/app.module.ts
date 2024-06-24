befcac8e2af06326fd3a91272685713c2655ace8
Karan Chaudhary <tharukaran930@gmail.com>
2024-06-24 16:24:00 +0545

    feat: changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+// import { UserController } from 'user/user.controller';
 
 @Module({
   imports: [PrismaModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  // controllers: [AppController, UserController],
+  // providers: [AppService],
 })
 export class AppModule {}
